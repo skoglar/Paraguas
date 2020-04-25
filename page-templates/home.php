c16a27ebda88af9bc5631b7a7d1c0749494af2f8
Pablo <pablorozasb@gmail.com>
2020-04-24 20:39:52 -0400

    changed home template name

--- page-templates/home.php
+++ page-templates/home.php
@@ -1,12 +1,8 @@
 <?php
 /**
- * The main template file.
+ * Template Name: Home Page
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ * Template for displaying a blank page.
  *
  * @package paraguas
  */
