77656cfb5f7a6a9a5cb7a6a019828734612f8b7d
Pablo <pablorozasb@gmail.com>
2020-04-25 01:57:23 -0400

    plantilla del blog ahora sin footer ni header

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -14,29 +14,29 @@ defined( 'ABSPATH' ) || exit;
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-	<header class="entry-header">
-
-		<?php
-		the_title(
-			sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-			'</a></h2>'
-		);
-		?>
+	<!-- <header class="entry-header">
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 
 			<div class="entry-meta">
 				<?php paraguas_posted_on(); ?>
-			</div><!-- .entry-meta -->
+			</div>
 
 		<?php endif; ?>
 
-	</header><!-- .entry-header -->
+	</header> -->
+	<!-- .entry-header -->
 	<div class="d-flex flex-row">
 		<div class="thumb-box">
 			<img class="blog-thumbnail" src="<?php echo get_the_post_thumbnail_url( $post->ID, 'large' ); ?>"  alt="{$post_title}">
 		</div>
 		<div class="entry-content blog-excerpt">
+		<?php
+			the_title(
+				sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+				'</a></h3>'
+			);
+		?>
 			<?php the_excerpt(); ?>
 			<?php
 			wp_link_pages(
@@ -50,10 +50,12 @@ defined( 'ABSPATH' ) || exit;
 		</div><!-- .entry-content -->
 	</div>
 
-
+	<!--
 	<footer class="entry-footer">
 		<?php paraguas_entry_footer(); ?>
 
-	</footer><!-- .entry-footer -->
+	</footer>
+	-->
+	<!-- .entry-footer -->
 
 </article><!-- #post-## -->
