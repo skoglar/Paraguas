8090c388eab1facb647958e25fb7b46b7720a43a
Pablo <pablorozasb@gmail.com>
2020-04-26 21:39:33 -0400

    sidebar removido del index

--- index.php
+++ index.php
@@ -29,9 +29,7 @@ $container = get_theme_mod( 'paraguas_container_type' );
 
 		<div class="row">
 
-			<!-- Do the left sidebar check and opens the primary div
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
- 			-->
+
 			<main class="site-main" id="main">
 				<?php
 					$the_query = new WP_Query( 
@@ -67,9 +65,7 @@ $container = get_theme_mod( 'paraguas_container_type' );
 			<!-- The pagination component -->
 			<?php paraguas_pagination(); ?>
 
-			<!-- Do the right sidebar check 
-			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
-			-->
+
 		</div><!-- .row -->
 
 	</div><!-- #content -->
