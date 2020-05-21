0acf2168a65b580403f6b090fdfd2aef7b654499
Pablo <pablorozasb@gmail.com>
2020-05-20 21:24:49 -0400

    get_content_by_type en paraguas customs

--- inc/paraguas-customs.php
+++ inc/paraguas-customs.php
@@ -49,7 +49,56 @@
             }
         endif;
     }
-
+    function tab_content_by_type($posts_type = 'post'){
+        // the query
+        $the_query = new WP_Query( array(
+            'post_type' => $posts_type,
+            'posts_per_page' => 3,
+        )); 
+        //The Body
+        if ( $the_query->have_posts() ) :
+            while ( $the_query->have_posts() ) : $the_query->the_post();
+                $post_link = get_permalink();
+                //$post_title = substr(get_the_title(), 0, 25);
+                $post_title = get_the_title();
+                $thumbnail_url = get_the_post_thumbnail_url();
+                $placeholder_thumb = 'https://images\.pexels\.com/photos/17739/pexels-photo\.jpg?auto=compress&cs=tinysrgb&h=750&w=1260';
+                $excerpt = get_the_excerpt();
+                $excerpt = substr($excerpt, 0, 60);
+                // if (strlen($post_title) > 35) 
+                // {
+                //     $post_title = wordwrap($post_title, 35);
+                //     $post_title = substr($post_title, 0, strpos($post_title, "\n"));
+                // }
+                echo <<<POSTS
+                <div class="card showcase-card">
+                    <div class="card-body">
+                        <a class="card-title" href={$post_link}>{$post_title}</a>
+                    </div>
+                    <img class="tab-thumbnail" src="{$thumbnail_url}" alt="Card image">
+                    <div class="card-body">
+                        <div class="card-body-container">
+                            <p class="card-text">{$excerpt}</p>
+                        </div>
+                    </div>
+                </div>
+                POSTS;
+            endwhile;
+            wp_reset_postdata();
+        else :
+            $placeholder_thumb = 'https://images.pexels.com/photos/17739/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=750&w=1260';
+            for ($i=0; $i < 3; $i++) { 
+                echo <<<POSTS
+                <div class="flex-fill tab-box">
+                <a href="/">Nada en esta sección</a><br>
+                <a href={$post_link}>
+                    <img class="tab-thumbnail" src={$placeholder_thumb} alt="No se encontraron posts">
+                </a>
+                </div>
+                POSTS;
+            }
+        endif;
+    }
     function carousel_content_by_tag($post_tag = 'uncategorized'){
         
             // Get 3 posts tagged with Novedades
