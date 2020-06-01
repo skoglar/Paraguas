ec364da1bd18af259d73edb3ee6a511eb85e1dae
Pablo <pablorozasb@gmail.com>
2020-06-01 19:18:06 -0400

    text archive minor fix

--- archive-sm_texto.php
+++ archive-sm_texto.php
@@ -33,7 +33,6 @@ $container = get_theme_mod( 'paraguas_container_type' );
 						
 						<?php
 						the_archive_title( '<h1 class="page-title">', '</h1>' );
-						the_archive_description( '<div class="taxonomy-description">', '</div>' );
 						?>
 					</header><!-- .page-header -->
 
