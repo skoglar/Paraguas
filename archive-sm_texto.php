b723c52c75e0538023b2ba4a390b20fa74d0f8da
Pablo <pablorozasb@gmail.com>
2020-06-01 19:22:39 -0400

    minor archive fix

--- archive-sm_texto.php
+++ archive-sm_texto.php
@@ -28,25 +28,15 @@ $container = get_theme_mod( 'paraguas_container_type' );
 
 				<?php if ( have_posts() ) : ?>
 
-					<header class="page-header">
-					
-						
+					<header class="page-header">	
 						<?php
 						the_archive_title( '<h1 class="page-title">', '</h1>' );
 						?>
 					</header><!-- .page-header -->
 
-					<?php /* Start the Loop */ ?>
 					<?php while ( have_posts() ) : the_post(); ?>
-						<?php echo get_post_type(); ?>
 						<?php
-
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'loop-templates/content', get_post_format() );
+							get_template_part( 'loop-templates/content', get_post_format() );
 						?>
 
 					<?php endwhile; ?>
