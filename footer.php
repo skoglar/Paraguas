d428e13ef4955fcaaab06bea713d7105d8f1726b
Pablo <pablorozasb@gmail.com>
2020-05-31 20:29:44 -0400

    footer

--- footer.php
+++ footer.php
@@ -23,14 +23,22 @@ $container = get_theme_mod( 'paraguas_container_type' );
 
 			<div class="col-md-12">
 
-				<footer class="site-footer" id="colophon">
-
-					<div class="site-info">
-
-						<!-- <?php paraguas_site_info(); ?> -->
-
+				<footer class="site-footer d-flex justify-content-between" id="colophon">
+					<div class="site-info align-self-start">
+						Editorial Invertido <i class="fa fa-copyright"></i> &mdash; 2020 &mdash; <a href="mailto:contacto@invertidoediciones.cl">contacto@invertidoediciones.cl</a>
 					</div><!-- .site-info -->
-
+					<div class="site-contact align-self-end">
+						<a href="https://www.instagram.com/invertidoediciones/">
+							<i class="fa fa-instagram fa-lg"></i>&nbsp;
+						</a>
+						<a href="https://www.facebook.com/Invertido-Ediciones-1118203675051290/">
+							<i class="fa fa-facebook fa-lg"></i>&nbsp;
+						</a>
+						<a href=""></a>
+						<a href="mailto:contacto@invertidoediciones.cl">
+							<i class="fa fa-envelope fa-lg"></i>
+						</a>
+					</div>
 				</footer><!-- #colophon -->
 
 			</div><!--col end -->
